7e17d3e3e4017db66490410898f88dda3e1a0ad9
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-03-01 22:23:20 +0000

    add courses stats path

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,5 +5,11 @@ class CoursesController < ApplicationController
     render json: Course.all.as_json(only: [:id,:name])
   end
 
+  def stats
+    @courses = Course.joins(:questions).merge(Question.asked_since(Date.today - 5.weeks))
+    @courses = @courses.group('courses.id').select('SUM(questions.votes) AS vote_count','name','id', 'COUNT(questions)')
+    render json: @courses.as_json
+  end
+
   def show; end
 end
