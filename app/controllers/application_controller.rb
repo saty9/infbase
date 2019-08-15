38409aa32bbe7917ba54daa3754a7e3887d0d1d1
kyle <cookekyle97@gmail.com>
2019-08-15 21:09:55 +0100

    throw 403s when an action is unauthorised

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::API
   respond_to :json
   include Pundit
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def render_resource(resource)
     if resource.errors.empty?
       render json: resource
@@ -25,5 +27,17 @@ class ApplicationController < ActionController::API
         }
       ]
     }, status: :bad_request
+  end
+
+  def user_not_authorized
+    render json: {
+        errors: [
+            {
+                status: '403',
+                title: 'You are not authorized to perform this action.',
+                code: '100'
+            }
+        ]
+    }, status: :forbidden
   end
 end
