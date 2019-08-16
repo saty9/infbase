501a6c3e17a4787a63e9933502010331bc030e38
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-08-16 18:33:22 +0100

    add teaching session authorisations

---
+++ app/policies/teaching_session_policy.rb
@@ -0,0 +1,29 @@
+class TeachingSessionPolicy < ApplicationPolicy
+  attr_reader :user, :teaching_session
+
+  def initialize(user, teaching_session)
+    @user = user
+    @teaching_session = teaching_session
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.role == "admin"
+  end
+
+  def update?
+    user.is_staff?
+  end
+
+  def destroy?
+    user.role == "admin"
+  end
+
+end
