7a4392f465c101ec25fdef591ca31ca99e509521
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-10-20 17:27:50 +0100

    add annotations to useful resource model

--- app/models/useful_resource.rb
+++ app/models/useful_resource.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: useful_resources
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  course_id  :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class UsefulResource < ApplicationRecord
   belongs_to :course
 end
